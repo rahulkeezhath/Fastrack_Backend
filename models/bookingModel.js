11dd6fae32c6451529566c5f92bcd67e3c64cce9
rahulkeezhath <rahulkeezhath.com>
2023-03-17 20:32:22 +0530

    Changed

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -13,4 +13,8 @@ const bookingSchema = mongoose.Schema ({
     shippingAddress: { name: { type: String }, email: { type: String }, address: { type:String}, city: { type: String}, pincode: { type: String }}
 }, { timestamps: true})
 
+
+
 module.exports = mongoose.model('Bookings', bookingSchema)
+
+
